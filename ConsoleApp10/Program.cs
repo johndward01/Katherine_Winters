418bb27a67ece304122643bdacb8709a2ee813c0
jdw <johndward01@gmail.com>
2021-12-13 16:07:47 -0600

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        // Main Method
+        static void Main(string[] args)
+        {
+            var result = Add(40, 40);
+            Console.WriteLine(result);
+
+            var strResult = PrintName("Katherine", "Winters");
+            Console.WriteLine(strResult);
+        }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static string PrintName(string firstName, string lastName)
+        {
+            return $"Hello, {firstName} {lastName}.";
+        }
+
+
+
+    }
+}
